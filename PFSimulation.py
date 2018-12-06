827f8ad920150d04b0e6ea3b66130d71baf65963
BC <zhang.beichen@dhs.sg>
2018-12-05 18:20:45 -0800

    Rewrite Partition Function computing function and theoretical Values.

--- PFSimulation.py
+++ PFSimulation.py
@@ -25,13 +25,44 @@ class SimpleHarmonicOscillatorPF:
 
     #Produce a graph with respect to beta
     #x: a serie of temperatures to be plotted against
-    def PF(self,x,graph = False):
-        beta = [ 1/Kb*v for v in x ]
-        y = [ 1/(np.exp(b*Heta*self.freq/2)-np.exp(-b*Heta*self.freq/2)) for b in beta]
+    #def PF(self,tmpRange,freq, interval = 50,maxn = 100,graph = True):
+     #   T = np.linspace( tmpRange[0], tmpRange[1], interval)
+      #  Freq: frequency of SHO. Note: due to accuracy of the small number, the summation is not accurate under lower frequency.The default freq is 300GHz at infra-red range
+    def PF_Theory(self,tmpRange,freq=3e12,interval = 50 ,graph = True):
+        T = np.linspace( tmpRange[0], tmpRange[1], interval)
+        omega = freq*2*np.pi
+        #k1 = beta* H_bar = 1.43878e-11 * 1/T
+        k1 = 1.43878e-11/T
+        P = np.exp(-0.5*k1*omega)/(1-np.exp(-k1*omega))
         if graph:
-            plt.plot(beta,y)
+            plt.plot(T,P)
             plt.show()
-        return y
+        return P
+    #
+    #Input: tmpRange = [lowtemp,hightemp]
+    #       interval = # of data points between these two intervals
+    #       freq = natural oscillation frequency
+    #       
+    #Unit: All energy is in unit of eV
+    def PF(self,tmpRange,freq=3e12, interval = 50,maxn = 1000000,graph = True):
+        T = np.linspace( tmpRange[0], tmpRange[1], interval)
+        omega = freq*2*np.pi
+        #k1 = /beta* H_bar = 1.43878e-11 * 1/T
+        k1 = 1.43878e-11/T
+        C1 = np.exp(-k1*omega/2)
+        C2 = np.exp(-k1*omega)
+
+        #P = C1*Sum(C2^n)
+        P = np.zeros(interval)
+        for n in range(interval):
+            P = P + np.power(C2,n)   
+        P = P * C1 
+         
+        #Plot Partition Function VS Temeprature
+        if graph:
+            plt.plot(T,P)
+            plt.show()
+        return P
 
     #
     #TODO:Consider Make FE,AE,S to be a generic function. Here, we use analytical Solution
@@ -78,7 +109,7 @@ class diatomicPF():
     #TODO: set to real values of E0 and R0
     E0 = 1.1
     R0 = 1
-    def __init__(self, maxJ = 100, tRange = [1 3000]):
+    def __init__(self, maxJ = 100, tRange = [1,3000]):
         self.maxJ = maxJ
         self.tRange = tRange
 
@@ -143,9 +174,11 @@ def demonstration():
     freq = 100
     nu = np.linspace(1, 10, num=100)
     sho1d = SimpleHarmonicOscillatorPF(freq)
-    sho1d.PF(nu)
-    sho1d.Entropy(nu)
-    sho1d.AE(nu)
+    sho1d.PF([100,373],1e12)
+    sho1d.PF_Theory([100,373],1e12)
+   #sho1d.PF_Theory(nu)
+    #sho1d.Entropy(nu)
+    #sho1d.AE(nu)
 
 
 if __name__ == "__main__":
